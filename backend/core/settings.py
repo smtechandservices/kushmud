da4202906c8ef0450054ed2c69b4a72fe543999c
satyaakant <satyakantmishra50@gmail.com>
2026-07-06 18:12:25 +0530

    setting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "kushmud",
+        "USER": "kushmud_user",
+        "PASSWORD": "Kushmud@123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
