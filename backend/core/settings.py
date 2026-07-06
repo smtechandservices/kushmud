68cf0e232c376e497c69e35d1747d6daa4a8a2b9
satyaakant <satyakantmishra50@gmail.com>
2026-07-06 18:56:23 +0530

    fass

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%fqx6i=3^ziez*l88nz@+8da#m-yszoy3b*)@!)is*l(sv&%7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
